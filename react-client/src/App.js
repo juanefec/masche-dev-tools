50684624426ff4595abefbca6d205a6ad29d1144
CLOT, JUAN <juanc@lagash.com>
2018-07-19 18:21:23 -0300

    Token generator in progress

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -3,19 +3,43 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  constructor(props) {
+    super(props);
+    this.state = { 
+      error: null,isLoaded: true,token: ''};
   }
+ 
+  getToken() {
+    fetch('http://localhost:3000/api/Token')
+    .then(res => res.json())
+    .then((result) => {
+      console.log(result);
+        this.setState({    isLoaded: false, token : result.token});
+    });
+  }
+
+  render() {
+    const { error, isLoaded, token } = this.state;
+    if(isLoaded)
+    {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+          <button onClick={this.getToken}>Generate Token</button>
+          </div>
+        );
+        } else {   
+        if(!isLoaded){
+        <div>{this.token}</div>
+      }  
+  }
+}
 }
 
 export default App;
--- server.go
+++ server.go
@@ -5,34 +5,35 @@ import (
 
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
+
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
 )
 
 func main() {
 	router := gin.Default()
-
 	// Serve frontend static files
 	router.Use(static.Serve("/", static.LocalFile("./react-client/build", true)))
 
 	// Setup route group for the API
 	api := router.Group("/api")
 	{
-		api.GET("/", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "pong",
-			})
-		})
+		api.GET("/Token", TokenHandler)
 	}
-	router.GET("/joke", JokeHandler)
+
 	router.POST("/jokeLike", LikeJoke)
 
 	// Start and run the server
 	router.Run(":3000")
 }
 
-func JokeHandler(c *gin.Context) {
+// TokenHandler returns a token as response
+func TokenHandler(c *gin.Context) {
+	body := getToken()
 	c.Header("Content-Type", "application/json")
 	c.JSON(http.StatusOK, gin.H{
-		"message": "Jokes handler not implemented yet",
+		"token": body,
 	})
 }
 
@@ -42,4 +43,31 @@ func LikeJoke(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "LikeJoke handler not implemented yet",
 	})
+}
+
+type Token struct {
+	Name   string
+	Token  string
+	UserID string
+}
+
+func getToken() string {
+	url := "http://webcalldesa02:9300/api/tokens"
+
+	var jsonStr = []byte(`{"username":"B048151","password":"juan1807"}`)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	var token Token
+	body, _ := ioutil.ReadAll(resp.Body)
+	json.Unmarshal(body, &token)
+	return token.Token
 }
