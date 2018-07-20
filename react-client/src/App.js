da1d41d0cbc5b54cb78228d5ca1eeaa2ea760dbc
Lucas Amado <lucasa@lagash.com>
2018-07-20 12:01:54 -0300

    feat: modify front end

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import Button from 'react-bootstrap/lib/Button'
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,17 +6,42 @@ class App extends Component {
     super(props);
     this.state = {
       isLoaded: false,
+      user: '',
+      password: '',
       token: ''
     };
 
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  getToken() {
-    fetch('http://localhost:3000/api/Token')
-    .then(res => res.json())
-    .then(({token}) => this.setState({
-      token,
-      isLoaded: true     
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    fetch('http://localhost:3000/api/token', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        user: this.state.user,
+        password: this.state.password
+      })
+    }).then((responseText) => responseText.json())
+    .then((response) => this.setState({
+      token: response
     }));
+    
+    event.preventDefault();
   }
 
   render() {
@@ -26,15 +49,22 @@ class App extends Component {
     if(!isLoaded) {
       return (
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-          <Button onClick={this.getToken}>Generate Token</Button>
-          </div>
+          <h1>MascheDevTools!!!!!!!</h1>
+          <img src="img/logo.png" alt="MascheDevTools"/>
+          <br/>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Usuario: 
+              <input type="text" name="user" value={this.state.user} onChange={this.handleInputChange} />
+            </label><br/>
+            <label>
+              Contraseña: 
+              <input type="password" name="password" value={this.state.password} onChange={this.handleInputChange} />
+            </label><br/>
+            <input type="submit" value="Submit" /><br/>
+            <label>{this.state.token}</label>
+            </form>
+        </div>
         );
     } else {   
         return <div>{token}</div> 
